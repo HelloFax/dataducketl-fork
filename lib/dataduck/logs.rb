6307c144bc0cb65bef318744b7512c6ca43d2d13
Michael Lindsay <michael.lindsay@hellosign.com>
2018-04-12 11:59:20 -0700

    Format dataduck logs in logstash friendly way Allow configuration of log location and format

--- lib/dataduck/logs.rb
+++ lib/dataduck/logs.rb
@@ -13,27 +13,44 @@ module DataDuck
       @@logger ||= Logger.new(log_file_path, shift_age = 100, shift_size = 100 * @@ONE_MB_IN_BYTES)
     end
 
+
+    def self.load_config!
+      logging_config = DataDuck.config['dataduck_logging']
+
+      log_file_path = DataDuck.project_root + '/log/dataduck.log'
+      log_datetime_format = '%Y/%m/%d %H:%M:%S'
+      unless logging_config.nil?
+        unless logging_config['path'].nil?
+          log_file_path = logging_config['path']
+        end
+        unless logging_config['datetime_format'].nil?
+          log_datetime_format = logging_config['datetime_format']
+        end
+      end
+
+      logger = Logger.new(log_file_path, shift_age = 100, shift_size = 100 * @@ONE_MB_IN_BYTES)
+      logger.formatter = proc do |severity, datetime, progname, msg|
+        "#{datetime.strftime(log_datetime_format)} [#{severity}] #{msg}\n"
+      end
+
+      @@logger = logger
+    end
+
     def Logs.debug(message)
       self.ensure_logger_exists!
       message = Logs.sanitize_message(message)
-
-      puts "[DEBUG] #{ message }"
       @@logger.debug(message)
     end
 
     def Logs.info(message)
       self.ensure_logger_exists!
       message = Logs.sanitize_message(message)
-
-      puts "[INFO] #{ message }"
       @@logger.info(message)
     end
 
     def Logs.warn(message)
       self.ensure_logger_exists!
       message = Logs.sanitize_message(message)
-
-      puts "[WARN] #{ message }"
       @@logger.warn(message)
     end
 
@@ -41,8 +58,6 @@ module DataDuck
       self.ensure_logger_exists!
       message = err.to_s unless message
       message = Logs.sanitize_message(message)
-
-      puts "[ERROR] #{ message }"
       @@logger.error(message)
 
       if ENV['SENTRY_DSN']
